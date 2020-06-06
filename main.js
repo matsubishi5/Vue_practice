715a2fd5bfaeccf28ea780b3ae596ba1cb409a35
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-06-06 18:46:26 +0900

    [learn]v-if

--- main.js
+++ main.js
@@ -2,13 +2,7 @@ new Vue({
   el: '#app',
 
   data: {
-    styleObject: {
-      color: 'red',
-      'background-color': 'blue',
-    },
-    baseObject: {
-      'font-size': '60px',
-    },
+    ok: true,
   },
 
   methods: {},
